fea571cd852ab014755cde074b996b8b1212d9be
Babu Rallapalli <baburallapalli@users.noreply.github.com>
2017-12-20 14:16:07 -0700

    Update create-ingress.sh

--- ingress/create-ingress.sh
+++ ingress/create-ingress.sh
@@ -14,10 +14,10 @@ sleep 10
 
 kubectl create sa oraclebmc-provisioner -n=$NAMESPACE
 
-kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-default-backend-deployment.yaml -n=$NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/baburallapalli/kubernetes/master/ingress/nginx-default-backend-deployment.yaml -n=$NAMESPACE
 
-kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-default-backend-service.yaml -n=$NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/baburallapalli/kubernetes/master/ingress/nginx-default-backend-service.yaml -n=$NAMESPACE
 
-kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-ingress-controller-deployment.yaml -n=$NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/baburallapalli/kubernetes/master/ingress/nginx-ingress-controller-deployment.yaml -n=$NAMESPACE
 
-kubectl create -f https://raw.githubusercontent.com/nagypeter/kubernetes/master/ingress/nginx-ingress-controller-service.yaml -n=$NAMESPACE
+kubectl create -f https://raw.githubusercontent.com/baburallapalli/kubernetes/master/ingress/nginx-ingress-controller-service.yaml -n=$NAMESPACE
